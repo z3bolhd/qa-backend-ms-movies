35c160c126d9e097364e35d2f4693a5906ae4d03
ZeBolHD <zezebolz@gmail.com>
2024-03-21 06:57:45 +0300

    chore: installed dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,33 @@
-/** @type {import("eslint").Linter.Config} */
-
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
