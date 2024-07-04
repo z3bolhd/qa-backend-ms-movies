c46652d754a72927a0b68c5d2b9862a40760f98f
ZeBolHD <zezebolz@gmail.com>
2024-07-04 18:56:36 +0300

    chore: testing setup

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest";
+import { compilerOptions } from "./tsconfig.json";
+import { pathsToModuleNameMapper } from "ts-jest";
+
+const config: Config = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: ".",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "./coverage",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src/", "<rootDir>/libs/"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+};
+
+export default config;
