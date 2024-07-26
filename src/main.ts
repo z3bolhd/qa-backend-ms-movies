c5e02fbe1276f912e02e988988c0470fe5e4eabb
ZeBolHD <zezebolz@gmail.com>
2024-07-26 19:18:33 +0300

    deploy: changed version

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle("Movies API")
     .setDescription("This API for movies")
-    .setVersion("1.06.1")
+    .setVersion("1.06.2")
     .addServer(HOST, "API server")
     .setExternalDoc("Коллекция json", HOST + "/swagger-json")
     .addBearerAuth()
